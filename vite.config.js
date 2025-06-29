496857e0b39f4cc8d4d8b52cf2edfe1d78004305
edouardo <edouardoaf2@gmail.com>
2025-06-29 16:45:42 +0100

    vite configs

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// Get the repository name from the homepage URL
+const REPO_NAME = 'Crackers-Dev-Portfolio'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: `/${REPO_NAME}/`,
 })
