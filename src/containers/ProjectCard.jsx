21fb24a3b86d42704d26b26967c5ab64acf4126b
edouardo <edouardoaf2@gmail.com>
2025-06-30 00:41:28 +0100

    projects section adding

--- src/containers/ProjectCard.jsx
+++ src/containers/ProjectCard.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
-export default function ProjectCard() {
+export default function ProjectCard({ 
+    title= "", 
+    desciption="",
+    link= "",
+    cover,
+    littleImg
+}) {
   return (
-    <div>
-        
+    <div className=' w-[75%] bg-gray-100 dark:bg-gray-900 rounded-xl'> {/* shadow-md shadow-blue-400 rounded-xl*/}
+        {/* IMAGES */}
+        <div className='relative inset-0'>
+          <div className=" h-40 sm:h-60 lg:h-90 overflow-hidden rounded-xl">
+            <img src={cover} alt={title} className='w-full h-full' />
+          </div>
+          <div className={" hidden md:absolute bottom-2 left-2 md:block w-15 h-20 overflow-hidden " + 
+            " border-2 border-blue-800 rounded-xl"}
+          >
+            <img src={littleImg} alt={title} className='w-full h-full' />
+          </div>
+        </div>
+        {/* text */}
+        <div className='px-3 py-5'>
+          <p className='font-semibold mb-3 text-2xl'>{title}</p>
+          <p className=' text-xs text-black dark:text-white'>{desciption}</p>
+        </div>
+        <button>
+            <a href={link} target="_blank" rel="noopener noreferrer">
+                Visiter
+            </a>
+        </button>
     </div>
   )
 }
--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -1,17 +1,62 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
+import voiture from '../assets/projects/ciryz_landing.png'
+import voitureLittle from '../assets/projects/ciryz_categorie.png'
+import graphik from '../assets/projects/Graphisme_site.png'
+import graphikLittle from '../assets/projects/Graphisme_vue.png'
+import conference from '../assets/projects/site_formations.png'
+import conferenceLittle from '../assets/projects/formation_leadership.png'
+import printer from '../assets/projects/Impression_sites.png'
+import printerLittle from '../assets/projects/Impressions_kakemono.png'
+import couture from '../assets/projects/uniforme_site.png'
+import coutureLittle from '../assets/projects/uniforme.png'
+import magasin from '../assets/projects/magasin_sites.png'
+import magasinLittle from '../assets/projects/magasin_1.png'
 
 export default function Projects() {
-    const services = [
+    const projects = [
         {
-          title: "Développement Web",
+          title: "Location de Véhicule",
           description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias vitae illum at non iure quam sunt illo ipsam facilis cum eligendi nobis odit quos nesciunt, explicabo voluptate possimus earum pariatur`,
-          link: "Test",
+          link: "https://www.ciryzcoated.com/",
+          cover: voiture,
+          littleImg: voitureLittle
         },
         {
-          title: "Développement Mobile",
+          title: "Agence de Graphisme",
           description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias vitae illum at non iure quam sunt illo ipsam facilis cum eligendi nobis odit quos nesciunt, explicabo voluptate possimus earum pariatur`,
-          link: "Test",
+          link: "https://www.graphikland-website.vercel.app/",
+          cover: graphik,
+          littleImg: graphikLittle
         },
+        {
+            title: "Conférence et Formation",
+            description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias vitae illum at non iure quam sunt illo ipsam facilis cum eligendi nobis odit quos nesciunt, explicabo voluptate possimus earum pariatur`,
+            link: "https://koaching.vercel.app/",
+            cover: conference,
+            littleImg: conferenceLittle
+          },
+          {
+            title: "Imprimerie",
+            description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias vitae illum at non iure quam sunt illo ipsam facilis cum eligendi nobis odit quos nesciunt, explicabo voluptate possimus earum pariatur`,
+            link: "https://guadeloupe-banderole-website.vercel.app/",
+            cover: printer,
+            littleImg: printerLittle
+          },
+          {
+            title: "Stylisme et Couture",
+            description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias vitae illum at non iure quam sunt illo ipsam facilis cum eligendi nobis odit quos nesciunt, explicabo voluptate possimus earum pariatur`,
+            link: "https://texma.fr/",
+            cover: couture,
+            littleImg: coutureLittle
+          },
+          {
+            title: "Gestion de Stock",
+            description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias vitae illum at non iure quam sunt illo ipsam facilis cum eligendi nobis odit quos nesciunt, explicabo voluptate possimus earum pariatur`,
+            link: "https://test-indus-wizeco-backoffice.vercel.app/",
+            cover: magasin,
+            littleImg: magasinLittle
+          },
       ]
       return (
         <div className='flex fiexd inset-0 z-1 w-full bg-base-100 text-base-content'>
@@ -19,22 +64,23 @@ export default function Projects() {
             <div className=' w-[90%] mx-auto text-center py-12 text-blue-800 dark:text-white '>
                 <h3 className='text-xl md:text-3xl font-semibold mb-4 text-blue-800'>Nos Projets </h3>
                 <p className="mb-4 px-4 md:px-20 text-xs md:text-[0.98rem] ">
-                    Nous développons les produits digitaux suivants :
+                    Nous comptons quelques plateformes à notre active. Nous vous les présentons pour vous donner
+                    un aperçu de nos travaux. Vous pouvez pour mieux les admirer en les visiant sur le web. 
                 </p>
-                <div className="flex flex-row flex-wrap gap-6 md:gap-18 p-4 items-center justify-center">
-                    {/* Collab é è à â ê ô */}
-                    <SerivicesCard title={services[0].title} desciption={services[0].description}>
-                        <div className="m-auto flex items-center justify-center h-12 w-12  rounded-full text-white">
-                         <p>Texte</p>
-                        </div>
-                    </ SerivicesCard>
-
-                    {/* fiabilité é è à â ê ô */}
-                    <SerivicesCard title={services[1].title} desciption={services[1].description}>
-                        <div className="m-auto flex items-center justify-center h-12 w-12 rounded-full text-white p-[1.02rem] ">
-                            <p>Texte</p>
-                        </div>
-                    </ SerivicesCard>
+                <div className="flex flex-col gap-6 md:gap-18 p-4 items-center justify-center">
+                    {
+                        projects.map( (project, index)=> {
+                            return (
+                                <ProjectCard key={index} title={project.title} 
+                                    desciption={project.description}
+                                    link={project.link}
+                                    cover={project.cover}
+                                    littleImg={project.littleImg}
+                    />
+                            )
+                        })
+                    }
+                    
                 </div>
             </div>
         </div>
